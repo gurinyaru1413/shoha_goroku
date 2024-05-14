86b5c3b3d5993ed553f93172eace852f39ff3c0d
gurinyaru1412 <104782558+gurinyaru1413@users.noreply.github.com>
2024-05-15 01:19:05 +0900

    Add files via upload

---
+++ game/script.js
@@ -0,0 +1,19 @@
+function nice() {
+    document.getElementById('nice').currentTime = 0; //連続クリックに対応
+    document.getElementById('nice').play(); //クリックしたら音を再生
+}
+
+function nante() {
+    document.getElementById('nante').currentTime = 0; //連続クリックに対応
+    document.getElementById('nante').play(); //クリックしたら音を再生
+}
+
+function kimoi() {
+    document.getElementById('kimoi').currentTime = 0; //連続クリックに対応
+    document.getElementById('kimoi').play(); //クリックしたら音を再生
+}
+
+function iiyo() {
+    document.getElementById('iiyo').currentTime = 0; //連続クリックに対応
+    document.getElementById('iiyo').play(); //クリックしたら音を再生
+}
